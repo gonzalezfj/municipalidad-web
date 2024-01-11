2faf909e347c43095daa60375e342a500fdf629a
Federico Valdez <valdezfede21@gmail.com>
2024-01-11 10:06:06 -0300

    home style

--- src/components/Opciones/Card.jsx
+++ src/components/Opciones/Card.jsx
@@ -10,49 +10,55 @@ export const Card = () => {
     <div className='grid gap-x-14 gap-y-6 grid-cols-2 sm:gap-x-4 sm:gap-y-4 sm:grid-cols-3 mt-32 mb-20'>
       <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col items-center justify-center'>
         <div className='w-36 h-36'>
-          <img src={boletas} className='w-full h-full object-cover' alt='Boletas' />
+          <img src={boletas} className='object-cover' alt='Boletas' />
         </div>
-        <div className='flex-1 w-40 bg-secondary'>
-          <h4 className='text-sm w-full h-full items-center justify-center flex'>PAGO DE BOLETAS</h4>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-sm w-full h-full items-center justify-center flex text-slate-50'>PAGO DE BOLETAS</h4>
         </div>
       </div>
 
       <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col items-center justify-center'>
         <div className='w-36 h-36'>
-          <img src={boletas2} className='w-full h-full object-cover' alt='Boletas' />
+          <img src={boletas2} className='object-cover' alt='Boletas' />
         </div>
-        <div className='flex-1 w-40 bg-secondary  items-center justify-center flex'>
-          <h4 className='text-xs ml-6 w-full h-full items-center flex'>ESTACIONAMIENTO CONTROLADO</h4>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-xs ml-6 w-full h-full items-center flex text-slate-50'>ESTACIONAMIENTO CONTROLADO</h4>
         </div>
       </div>
 
       <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col justify-center items-center'>
         <div className='w-36 h-36'>
-          <img src={boletas3} className='w-full h-full object-cover items-center justify-center' alt='Boletas2' />
+          <img src={boletas3} className='object-cover' alt='Boletas2' />
         </div>
-        <div className='flex-1 w-40 bg-secondary flex items-center justify-center'>
-          <h4 className='text-base'>EMPLEO</h4>
-        </div>
-      </div>
-
-      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col'>
-        <img src={boletas4} className='w-full h-36 object-cover' alt='Boletas3' />
-        <div className='flex-1 bg-secondary flex items-center justify-center'>
-          <h4 className='text-base'>GESTION DE TRAMITES</h4>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-base text-slate-50'>EMPLEO</h4>
         </div>
       </div>
 
-      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col'>
-        <img src={boletas5} className='w-full h-36 object-cover' alt='Boletas4' />
-        <div className='flex-1 bg-secondary flex items-center justify-center'>
-          <h4 className='text-base'>ESTERILIZACIÓN DE MASCOTAS</h4>
+      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col items-center justify-center'>
+        <div className='w-36 h-36 mr-6'>
+          <img src={boletas4} className='object-cover' alt='Boletas3' />
+        </div>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-sm ml-10 text-slate-50'>GESTION DE TRAMITES</h4>
         </div>
       </div>
 
-      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col'>
-        <img src={boletas6} className='w-full h-36 object-cover ' alt='Boletas5' />
-        <div className='flex-1 bg-secondary flex items-center justify-center'>
-          <h4 className='text-base'>RECLAMOS</h4>
+      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col items-center justify-center'>
+        <div className='w-36 h-36'>
+          <img src={boletas5} className='object-cover' alt='Boletas4' />
+        </div>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-xs ml-6 text-slate-50'>ESTERILIZACIÓN DE MASCOTAS</h4>
+        </div>
+      </div>
+
+      <div className='w-40 h-52 overflow-hidden shadow-lg bg-third rounded-primary flex flex-col items-center justify-center'>
+        <div className='w-36 h-36'>
+          <img src={boletas6} className='mt-6 object-cover' alt='Boletas5' />
+        </div>
+        <div className='flex-1 w-40 flex items-center justify-center transition ease-in-out delay-150 bg-secondary hover:bg-purple-600 duration-200'>
+          <h4 className='text-base text-slate-50'>RECLAMOS</h4>
         </div>
       </div>
     </div>
