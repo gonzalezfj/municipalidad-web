1ef4f6063e238678dfc13fa1d2df927b6ff8bdb3
FacuJ440 <facujaled@outlook.com>
2024-01-11 09:06:24 -0300

    Submenu

---
+++ src/components/Navbar/Links.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+// import { NavLink } from 'react-router-dom'
+import { MdArrowBackIos } from 'react-icons/md'
+import Sublinks from './Sublinks'
+
+const Links = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <div className='container'>
+      <div className='flex mr-10'>
+        <button onClick={() => setOpen(true)} className='text-[#4B0984] flex items-center justify-between text-xl w-full py-3 mb-2'>
+          <MdArrowBackIos className='ml-10' />
+          Municipio
+        </button>
+      </div>
+      <div className='flex mr-10'>
+        <button className='text-[#4B0984] flex items-center justify-end text-xl w-full py-3 mb-2'>
+          Servicios
+        </button>
+      </div>
+      <div className='flex mr-10'>
+        <button className='text-[#4B0984] flex items-center justify-between text-xl w-full py-3 mb-2'>
+          <MdArrowBackIos className='ml-10' />
+          Cultura y Turismo
+        </button>
+      </div>
+      <div className='flex mr-10'>
+        <button className='text-[#4B0984] flex items-center justify-between text-xl w-full py-3 mb-2'>
+          <MdArrowBackIos className='ml-10' />
+          Transparencia
+        </button>
+      </div>
+      <div className='flex mr-10'>
+        <button className='text-[#4B0984] flex items-center justify-end text-xl w-full py-3 mb-2'>
+          Noticias
+        </button>
+      </div>
+      {/* Acá empieza el Sidebar */}
+      <div className={`${!open && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm`} onClick={() => setOpen(false)}> </div>
+
+      <div className={`${open ? 'translate-x-0' : 'translate-x-80'} bg-white min-h-screen w-80 fixed top-0 right-0 transition-all duration-300`}>
+        <div className={`${!open && 'hidden'} pt-3`}>
+          <button className=' ml-4 text-[#4B0984]' onClick={() => setOpen(false)}>
+            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+              <path strokeLinecap='round' strokeLinejoin='round' d='M6 18L18 6M6 6l12 12' />
+            </svg>
+          </button>
+          <Sublinks />
+        </div>
+      </div>
+    </div>
+
+  // <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Municipio</div>
+  // <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Servicios</div>
+  // <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Transparencia</div>
+  // <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Noticias</div>
+  // <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Contacto</div>
+  )
+}
+
+export default Links
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
+import Links from './Links'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div className='bg-white py-3 sticky top-0 left-0 right-0 shadow-md rounded-full flex justify-end'>
+    <div className='bg-white py-3 sticky top-0 left-0 right-0 shadow-md rounded-full flex justify-between'>
+      <NavLink to='/home'><img className='px-3' src='src\assets\capital_sanjuan_blanco.png' /></NavLink>
       <button className='mr-4 mt-1' onClick={() => setOpen(true)}>
-        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
+        <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6 mr-1'>
           <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5' />
         </svg>
       </button>
 
       <div className={`${!open && 'hidden'} bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm`} onClick={() => setOpen(false)}> </div>
 
-      <div className={`${open ? 'w-80' : 'w-0'} bg-white min-h-screen fixed top-0 right-0 transition-all duration-300`}>
+      <div className={`${open ? 'translate-x-0' : 'translate-x-80'} bg-white min-h-screen w-80 fixed top-0 right-0 transition-all duration-300`}>
         <div className={`${!open && 'hidden'} pt-3`}>
           <button className=' ml-4 text-[#4B0984]' onClick={() => setOpen(false)}>
             <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
@@ -22,11 +25,7 @@ const Navbar = () => {
           <div className='flex justify-center'>
             <input placeholder='Buscar...' className='p-2 mb-2 w-60 rounded-full border border-[#4B0984]' />
           </div>
-          <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Municipio</div>
-          <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Servicios</div>
-          <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Transparencia</div>
-          <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Noticias</div>
-          <div className='text-right text-[#4B0984] text-xl cursor-pointer py-3 mb-2 mr-10'>Contacto</div>
+          <Links />
         </div>
         <img src='src\assets\group_61.png' className='absolute bottom-20 right-0' />
       </div>
