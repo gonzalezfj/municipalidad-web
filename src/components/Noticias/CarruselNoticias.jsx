eef879ab800aedd08344e91c5558d4d3d9d5d73e
fvaldezz96 <valdezfede21@gmail.com>
2024-01-17 10:59:13 +0100

    correcion carrusel mobil

--- src/components/Noticias/CarruselNoticias.jsx
+++ src/components/Noticias/CarruselNoticias.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
-// import { ChevronLeft, ChevronRight } from 'react-feather';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { NoticiasMobil } from './NoticiasMobil';
@@ -39,24 +38,26 @@ export default function CarruselNoticias({
       {formato ? (
         <NoticiasMobil />
       ) : (
-        <div>
+        <div className='overflow-hidden items-center justify-center bg-white rounded-lg'>
           <Slider {...settings}>
-            {slides.map((img, key) => (
+            {slides.map((img, key, index) => (
               <div
                 data-aos="fade-left"
                 key={key}
-                className='w-screen flex-shrink-0 rounded-lg overflow-hidden shadow-lg lg:h-full bg-white'
+                className='w-screen flex-shrink-0 rounded-lg overflow-hidden shadow-lg lg:h-full '
               >
-                <div className='h-82 lg:h-96 rounded-lg flex object-cover'>
+                <div className='h-82 lg:h-96 rounded-lg flex object-cover '>
                   <img className='w-full h-64 lg:h-full object-cover rounded-lg' src={img} alt={`Slide ${key}`} />
                 </div>
-                <div className='px-6 py-4'>
-                  <div className='font-bold text-3xl text-fifth'>Pirotecnia Cero en la Ciudad de San Juan</div>
-                </div>
-                <div className='px-6 pt-2 pb-2 rounded-xl'>
-                  <span className='inline-block bg-gray-50 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 shadow-xl'>
-                    + info
-                  </span>
+                <div className='pl-2'>
+                  <div className=' py-4'>
+                    <div className='font-bold text-3xl text-fifth text-left'>Pirotecnia Cero en la Ciudad de San Juan</div>
+                  </div>
+                  <div className=' pt-2 pb-2 rounded-xl'>
+                    <span className='inline-block font-bold bg-six rounded-full px-3 py-1 shadow-lg text-sm text-gray-700 mr-2 mb-2'>
+                      + info
+                    </span>
+                  </div>
                 </div>
               </div>
             ))}
@@ -65,8 +66,8 @@ export default function CarruselNoticias({
             data-aos="fade-right"
             className='container items-center justify-center flex pt-10 pb-10'
           >
-            <span className='items-center justify-center flex bg-six rounded-3xl w-40 h-10 transform hover:translate-y-1 transition-transform shadow-xl'>
-              <h2 className='text-white items-center justify-center font-bold'>Más Noticias</h2>
+            <span className='items-center justify-center flex bg-orange-400 rounded-3xl w-40 h-10 shadow-lg transform hover:translate-y-1 transition-transform'>
+              <h2 className='text-gray-600 items-center justify-center '>Mostrar más ...</h2>
             </span>
           </div>
         </div>
