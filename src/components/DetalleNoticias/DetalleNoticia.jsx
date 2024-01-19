5f5e6abc302629ccbe2cb0c7380535b13842692d
fvaldezz96 <valdezfede21@gmail.com>
2024-01-19 13:09:44 +0100

    componente detalle listo

--- src/components/DetalleNoticias/DetalleNoticia.jsx
+++ src/components/DetalleNoticias/DetalleNoticia.jsx
@@ -7,8 +7,8 @@ export const DetalleNoticia = () => {
         <div className='container'>
             <div className='p-8 rounded-t-lg'>
                 <img className='rounded-t-lg pb-2' src={img} alt="" />
-                <h5 className=''>Jueves 21 Diciembre 2024</h5>
-                <h1 className='font-bold text-2xl text-roommn '>Pirotecnia Cero en la Ciudad de San Juan</h1>
+                <h5 className='font-semibold text-six'>Jueves 21 Diciembre 2024</h5>
+                <h1 className='font-bold text-3xl text-secondary'>Pirotecnia Cero en la Ciudad de San Juan</h1>
                 <p className=''>
                     En un esfuerzo por garantizar la seguridad y el bienestar de los residentes de la Ciudad de San Juan, el gobierno municipal ha anunciado que la ordenanza que prohíbe la venta de pirotecnia continúa vigente. <br />Esta medida tiene como objetivo principal resguardar la salud de las personas con autismo y de las queridas mascotas durante las festividades de fin de año y otras ocasiones especiales.
                     La ordenanza, establece una serie de medidas estrictas para disuadir la venta y el uso de productos pirotécnicos en el área urbana de la Ciudad San Juan. El incumplimiento de estas disposiciones resultará en severas sanciones para los infractores.
@@ -21,14 +21,17 @@ export const DetalleNoticia = () => {
                     <br />
                     Para asegurarse de que la ordenanza se cumpla a cabalidad, el municipio ha anunciado un aumento en las operaciones de control y vigilancia durante la temporada de festividades. Equipos de inspectores municipales serán los encargados de monitorear la venta y el uso de pirotecnia, y tomarán medidas inmediatas contra aquellos que la utilicen de manera indebida.
                 </p>
+                <h4 className='font-semibold text-six mt-4'>Modificado por última vez en Jueves, 21 Diciembre 2023 10:30
+                    Publicado en Noticias
+                </h4>
             </div>
             {/* BUSCAR NOTICIAS*/}
-            <div className='p-4'>
-                <label className=''>Buscar Noticias</label>
-                <input className='bg-fifth rounded-lg' type="text" />
+            <div className='bg-amber-400 h-36 flex flex-col items-center justify-center rounded-room'>
+                <label className='text-white text-2xl'>Buscar Noticias</label>
+                <input className='rounded-lg border-slate-200 mt-2' type="text" />
             </div>
-            <div className=''>
-                <h2 className=''>NOTICIAS RELACIONADAS</h2>
+            <div className='mt-10'>
+                <h2 className='font-bold text-1xl ml-10'>NOTICIAS RELACIONADAS</h2>
                 {/* COMPONENTE */}
                 <NoticiasRelacionadas />
             </div>
--- src/components/DetalleNoticias/NoticiasRelacionadas.jsx
+++ src/components/DetalleNoticias/NoticiasRelacionadas.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import img1 from '../../assets/home_seccion3_circuito_foto.jpg'
+import img2 from '../../assets/home_seccion3_agenda_foto.jpg'
+import img3 from '../../assets/home_seccion3_deporteysalud_foto.jpg'
 export const NoticiasRelacionadas = () => {
     return (
-        <div className=''>
-            NoticiasRelacionadas
+        <div className='w-62 p-4 items-center justify-center'>
+            <div className='flex flex-col-2 gap-4 p-4'>
+                <div>
+                    <img className='w-96 h-20 object-cover' src={img1} alt="" />
+                </div>
+                <p className='text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut tempora optio amet dignissimos placeat...</p>
+            </div>
+            <div className='w-auto h-1 bg-zinc-400'></div>
+            <div className='flex flex-col-2 gap-4 p-4'>
+                <div>
+                    <img className='w-96 h-20 object-cover' src={img2} alt="" />
+                </div>
+                <p className='text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut tempora optio amet dignissimos placeat...</p>
+            </div>
+            <div className='w-auto h-1 bg-zinc-400'></div>
+            <div className='flex flex-col-2 gap-4 p-4'>
+                <div>
+                    <img className='w-96 h-20 object-cover' src={img3} alt="" />
+                </div>
+                <p className='text-sm'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut tempora optio amet dignissimos placeat...</p>
+            </div>
         </div>
     )
 }
