af5d7be8218c66c58aeb0eef1df44ad38585a5fe
FacuJ440 <facujaled@outlook.com>
2024-01-25 13:12:52 -0300

    playa-desktop-terminado

--- src/components/PlayaRemocion/BannerPlayaRemocion.jsx
+++ src/components/PlayaRemocion/BannerPlayaRemocion.jsx
@@ -4,7 +4,7 @@ const BannerPlayaRemocion = () => {
   return (
     <div className=''>
       <div className='bg-primary absolute top-0 w-full h-44'>
-        <div className='text-[#4B0984] container mx-auto flex flex-col items-center justify-center p-20'>
+        <div className='text-[#4B0984] container mx-auto flex flex-col items-center justify-center p-20 lg:py-28'>
           <h1 className='font-extrabold'>PLAYA DE REMOCIÓN</h1>
           <p className=''>Consultá si tu vehículo fue remolcado por la Grúa Municipal.</p>
         </div>
