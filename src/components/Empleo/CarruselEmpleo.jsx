e3379cdec08685f9c99afb2691a665ea95906449
fvaldezz96 <valdezfede21@gmail.com>
2024-01-25 21:39:22 +0100

    cambios/desktop-Empleo

--- src/components/Empleo/CarruselEmpleo.jsx
+++ src/components/Empleo/CarruselEmpleo.jsx
@@ -20,27 +20,52 @@ export default function CarruselEmpleo({
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 1,
+        slidesToShow: 6,
         slidesToScroll: 1,
         swipeToSlide: true,
         autoplay: autoSlide,
         autoplaySpeed: autoSlideInterval,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                    variableWidth: true,
+                    centerMode: true
+                }
+            }
+        ]
     };
     return (
-        <div className='overflow-hidden items-center z-10 justify-center'>
+        <div className='cursor-pointer overflow-hidden z-10 lg:w-full'>
             <Slider {...settings}>
                 {slides.map((img, key, index) => (
                     <div
                         data-aos="fade-left"
                         key={key}
-                        className=' w-screen flex-shrink-0 rounded-lg overflow-hidden p-10 lg:h-full'
+                        className='w-full lg:w-60 flex rounded-lg overflow-hidden p-10 lg:h-full'
                     >
-                        <div className='h-82 lg:h-96 rounded-lg flex object-cover'>
+                        <div className='h-82 lg:h-40 lg:w-40 rounded-lg flex object-cover'>
                             <img className='w-full h-64 lg:h-full object-cover rounded-t-lg' src={img} alt={`Slide ${key}`} />
                         </div>
-                        <div className=' bg-secondary rounded-b-lg shadow-lg'>
-                            <div className='px-6 py-10'>
-                                <h3 className='font-bold text-3xl text-third'>Pirotecnia Cero en la Ciudad de San Juan</h3>
+                        <div className=' bg-secondary rounded-b-lg lg:w-40 lg:h-20 shadow-lg'>
+                            <div className='px-6 py-10 lg:px-0 lg:py-0 lg:w-60 lg:text-center'>
+                                <h3 className='font-bold text-3xl lg:w-40 lg:pt-4 lg:text-sm text-third'>Pirotecnia Cero en la Ciudad de San Juan</h3>
                             </div>
                         </div>
                     </div>
