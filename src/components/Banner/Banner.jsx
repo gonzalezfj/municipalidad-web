e00e67a69b8ad43786d091bf286778424a13bfc4
FacuJ440 <facujaled@outlook.com>
2024-01-29 08:41:18 -0300

    correción-video

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,7 +3,7 @@ import img from '../../assets/homemovile_seccion1_foto1.jpg'
 import video from '../../assets/home_seccion1_video.mp4'
 export const Banner = () => {
   return (
-    <div className='absolute top-0'>
+    <div className='absolute top-0 lg:h-96'>
       <video className='hidden relative -top-[56vh] lg:block' autoPlay loop muted>
         <source src={video} type='video/mp4' />
       </video>
