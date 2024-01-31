a84c2d4dadccf5e12b781438d6f8ef7f8d5bffd3
FacuJ440 <facujaled@outlook.com>
2024-01-31 10:51:57 -0300

    fix-carousel-noticias&destacado

--- src/components/Noticias/Noticias.jsx
+++ src/components/Noticias/Noticias.jsx
@@ -7,7 +7,7 @@ import img4 from '../../assets/homemovil_seccion4_fondo.svg'
 export const Noticias = () => {
   const slides = [img1, img2, img3]
   return (
-    <div className='flex flex-col top-40 items-center justify-center'>
+    <div className='flex relative flex-col items-center justify-center'>
       <h3 className='mb-8 mt-10 text-3xl text-gray-500 font-bold'>NOTICIAS</h3>
       <svg className='absolute -z-10 lg:hidden top-10 md:top-2 w-full h-96 md:h-96'>
         <image className='relative w-full' href={img4} />
