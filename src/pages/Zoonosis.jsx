838fae2436dae254fb696e525b499cee056ef6c4
FacuJ440 <facujaled@outlook.com>
2024-02-01 08:54:03 -0300

    fix-zoonosis

--- src/pages/Zoonosis.jsx
+++ src/pages/Zoonosis.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import TextZoonosis from '../components/Zoonosis/TextZoonosis'
 import ButtonTurnos from '../components/Zoonosis/ButtonTurnos'
-import ButtonTurnosPropios from '../components/Zoonosis/ButtonTurnosPropios'
 import fondo from '../assets/fondo_zoonosis.svg'
+import ButtonAtras from '../components/Default/ButtonAtras.jsx'
 
 const Zoonosis = () => {
   return (
-    <div className='relative pt-24'>
-      <div className='container mx-auto'>
+    <div className='relative pt-24 lg:pt-40'>
+      <div className='container mx-auto lg:flex lg:flex-row'>
         <TextZoonosis />
         <ButtonTurnos />
-        <ButtonTurnosPropios />
       </div>
-      <div className='absolute -bottom-20'>
-        <svg className='h-32 w-32'>
+      <div className='container mx-auto flex justify-center lg:justify-normal'>
+        <ButtonAtras />
+      </div>
+      <div className='absolute -bottom-20 lg:top-0 lg:right-0'>
+        <svg className='h-32 w-32 lg:rotate-180 lg:w-52 lg:h-52 xl:w-80 xl:h-80'>
           <image className='w-full' href={fondo} />
         </svg>
       </div>
     </div>
-
   )
 }
 
