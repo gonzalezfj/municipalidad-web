9cd20707449ce575f5fb64a7242d042ad8fba731
Federico Valdez <valdezfede21@gmail.com>
2024-02-21 14:23:17 -0300

    feat: desktop obras

--- src/components/Obras/InstalacionesElectricas.jsx
+++ src/components/Obras/InstalacionesElectricas.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ButtonBack from '../Default/ButtonBack'
-import img5 from '@assets/Libre Deuda/LibreDeuda_fondo.svg'
+import img1 from '@assets/Libre Deuda/LibreDeuda_fondo.svg'
+import img2 from '@assets/obras/obras_icono.svg'
 
 export const InstalacionesElectricas = () => {
   return (
@@ -15,18 +16,23 @@ export const InstalacionesElectricas = () => {
         <div className='pt-44 gap-10 flex flex-col text-[#3E3E3E] font-semibold'>
           {/* PARRAFOS */}
           <div className='flex flex-col gap-4 lg:px-96 px-8 lg:text-2xl'>
-            <h1 className='text-1xl font-extrabold text-[#4B0984] lg:items-center lg:justify-center lg:flex lg:w-1/3 lg:text-3xl'>BOLETAS PARA EL PAGO DE CONTRIBUCIONES SOBRE INSTALACIONES ELÉCTRICAS</h1>
-            <p>
+            <h1 className='text-1xl font-extrabold text-[#4B0984] lg:items-center lg:justify-center lg:flex lg:w-2/3 lg:text-3xl'>BOLETAS PARA EL PAGO DE CONTRIBUCIONES SOBRE INSTALACIONES ELÉCTRICAS</h1>
+            <p className='lg:text-xl'>
               Desde esta sección se podrán descargar las boletas para el pago de las contribuciones eléctricas.
             </p>
             <section className='gap-4 flex flex-col-2'>
-              <input type='text' placeholder='Increse la Nomenclatura o Inscripción' className='w-60 h-10 bg-[#E6E6E6] rounded-xl py-4 px-4 text-slate-800' />
+              <input type='text' placeholder='Increse la Nomenclatura o Inscripción' className='w-60 lg:w-2/5 h-10 bg-[#E6E6E6] rounded-xl py-4 px-4 text-slate-800' />
               <button className='rounded-xl bg-[#9C9C9C] px-4 text-white'>Iniciar</button>
             </section>
           </div>
           <div className='relative z-0'>
-            <svg className='h-48 w-48 lg:h-60 lg:w-60 absolute -top-[10px] -right-0 lg:-top-[90px] rotate-180 scale-y-[-1]'>
-              <image href={img5} alt='img not found' className='w-auto lg:w-96 lg:h-full' />
+            <svg className='h-32 w-32 lg:h-60 lg:w-60 absolute -top-[10px] -right-0 lg:-top-[280px] rotate-180 scale-y-[-1]'>
+              <image href={img1} alt='img not found' className='w-20 lg:w-96 lg:h-full' />
+            </svg>
+          </div>
+          <div className='relative z-0'>
+            <svg className='h-48 w-48 lg:h-64 lg:w-64 absolute -top-[10px] right-32 lg:right-96 lg:-top-[20px] rotate-180 scale-y-[-1]'>
+              <image href={img2} alt='img not found' className='w-auto lg:w-64 lg:h-64' />
             </svg>
           </div>
         </div>
