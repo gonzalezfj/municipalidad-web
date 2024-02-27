3bde0064e776fc2f33ef92ff5581b5b1f6341b44
FacuJ440 <facujaled@outlook.com>
2024-02-27 08:58:51 -0300

    feat: create page playaestacionamiento

--- src/routes/ComercioRoutes/ComercioRoutes.jsx
+++ src/routes/ComercioRoutes/ComercioRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router'
 import ManipulacionAlimentos from '../../components/Comercio/ManipulacionAlimentos'
 import Galerias from '../../components/Comercio/Galerias'
 import Geriatricos from '../../components/Comercio/Geriatricos'
+import PlayaEstacionamiento from '../../components/Comercio/PlayaEstacionamiento'
 
 const ComercioRoutes = () => {
   return (
@@ -9,7 +10,7 @@ const ComercioRoutes = () => {
       <Route path='/manipulacionalimentos' element={<ManipulacionAlimentos />} />
       <Route path='/galerias' element={<Galerias />} />
       <Route path='/geriatricos' element={<Geriatricos />} />
-      {/* <Route path='/detalle' element={<DetalleNoticia />} /> */}
+      <Route path='/playaestacionamiento' element={<PlayaEstacionamiento />} />
       {/* <Route path='/tramites' element={<Tramites />} /> */}
       {/* <Route path='/detalleagenda' element={<NuestraAgenda />} /> */}
       {/* <Route path='/estacionamientocontrolado' element={<Eco />} /> */}
