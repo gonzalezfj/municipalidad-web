617769670d919b73d2ee9ccc1989cca7a0fcc982
FacuJ440 <facujaled@outlook.com>
2024-02-27 08:47:18 -0300

    feat: create page geriatricos

--- src/routes/ComercioRoutes/ComercioRoutes.jsx
+++ src/routes/ComercioRoutes/ComercioRoutes.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router'
 import ManipulacionAlimentos from '../../components/Comercio/ManipulacionAlimentos'
 import Galerias from '../../components/Comercio/Galerias'
+import Geriatricos from '../../components/Comercio/Geriatricos'
 
 const ComercioRoutes = () => {
   return (
     <Routes>
       <Route path='/manipulacionalimentos' element={<ManipulacionAlimentos />} />
       <Route path='/galerias' element={<Galerias />} />
-      {/* <Route path='/empleo' element={<Empleo />} /> */}
+      <Route path='/geriatricos' element={<Geriatricos />} />
       {/* <Route path='/detalle' element={<DetalleNoticia />} /> */}
       {/* <Route path='/tramites' element={<Tramites />} /> */}
       {/* <Route path='/detalleagenda' element={<NuestraAgenda />} /> */}
