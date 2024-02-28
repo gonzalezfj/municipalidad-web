e52058e849ee4666f7a42236bf77216dddb8800f
Nahuel Gomez <nahuelgomezs@outlook.com>
2024-02-27 22:01:16 -0300

    feature: add slugify redirection, format date and image source from Strapi

--- src/pages/Noticias/Card/Card.jsx
+++ src/pages/Noticias/Card/Card.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import { slugify, formatDate } from '@utils'
 import { FaPlusCircle } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 
-const Card = ({ image, date, title, description, redirection }) => {
+const Card = ({ imageUrl, date, title, description }) => {
   const navigate = useNavigate()
 
   const handleClick = () => {
-    navigate(redirection)
+    const slugifyTitle = slugify(title)
+    console.log(slugifyTitle)
+    navigate(slugifyTitle)
   }
 
   return (
     <div className='flex flex-col gap-3'>
       <div className='lg:flex lg:flex-col-2 lg:gap-8'>
         {/* IMAGE */}
-        <img src={image} alt='img not found' className='lg:w-[30rem] rounded-2xl object-cover' />
+        <img src={import.meta.env.VITE_STRAPI_URL + imageUrl} alt='img not found' className='lg:w-[30rem] rounded-2xl object-cover' />
         {/* CONTENT */}
         <div className='lg:w-[30rem] lg:h-[10rem]'>
-          <h4 className='text-[#8A43EF] font-bold lg:text-2xl'>{date}</h4>
+          <h4 className='text-[#8A43EF] font-bold lg:text-2xl'>{formatDate(date)}</h4>
           <h1 className='font-extrabold text-[#4B0984] text-[1rem] lg:text-[1.2rem]'>{title}</h1>
           <p className='lg:text-[1.3rem]'>{description}</p>
           {/* BUTTON (+INFO) */}
