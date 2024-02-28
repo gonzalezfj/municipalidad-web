ca66971b516850f872d125bc44712392c617e5ea
Nahuel Gomez <nahuelgomezs@outlook.com>
2024-02-28 20:45:23 -0300

    feat: add data fetching from Strapi into news detail content

--- src/pages/Noticias/DetalleNoticias/DetalleNoticia.jsx
+++ src/pages/Noticias/DetalleNoticias/DetalleNoticia.jsx
@@ -1,31 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+import { useParams } from 'react-router'
+import { formatDate } from '@utils'
 import { NoticiasRelacionadas } from '../NoticiasRelacionadas'
-import img from '@assets/home_seccion4_noticiaPirotecnia.jpg'
 
 export const DetalleNoticia = () => {
+  const [newContent, setNewContent] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const getNewContent = async () => {
+      try {
+        const params = new URLSearchParams({
+          populate: '*'
+        })
+        const response = await fetch(import.meta.env.VITE_STRAPI_URL + '/api/noticias/' + id + '?' + params)
+        const newData = await response.json()
+        console.log(newData.data.attributes)
+        setNewContent(newData.data.attributes)
+      } catch (error) {
+        console.error('Error happened while fetching', error)
+      }
+    }
+    getNewContent()
+  }, [id])
+
+  if (Object.keys(newContent).length === 0) {
+    return <p>Cargando Noticia...</p>
+  }
+
   return (
     <div className='container lg:mx-auto'>
       {/* IMG BODY */}
       <div className='lg:flex lg:flex-col-2 lg:gap-3 lg:pt-[6rem]'>
         <div className='p-8 rounded-t-lg lg:w-[90rem]'>
-          <img className='rounded-t-lg pb-2' src={img} alt='img not fount' />
-          <h5 className='font-semibold text-six'>Jueves 21 Diciembre 2024</h5>
-          <h1 className='font-extrabold text-3xl text-secondary lg:w-[20rem]'>Pirotecnia Cero en la Ciudad de San Juan</h1>
-          <h4 className='italic font-semibold text-six'>Medidas para resguardar la salud de los vecinos y las mascotas.</h4>
-          <p className='pt-6'>
-            En un esfuerzo por garantizar la seguridad y el bienestar de los residentes de la Ciudad de San Juan, el gobierno municipal ha anunciado que la ordenanza que prohíbe la venta de pirotecnia continúa vigente. Esta medida tiene como objetivo principal resguardar la salud de las personas con autismo y de las queridas mascotas durante las festividades de fin de año y otras ocasiones especiales.
-
-            La ordenanza, establece una serie de medidas estrictas para disuadir la venta y el uso de productos pirotécnicos en el área urbana de la Ciudad San Juan. El incumplimiento de estas disposiciones resultará en severas sanciones para los infractores.
-
-            Una de las medidas más destacadas de la ordenanza es la imposición de multas significativas a quienes violan la prohibición. Los infractores se enfrentarán a multas que oscilan entre trescientos (300) y seiscientos (600) litros de nafta, lo que refleja la gravedad con la que el municipio se toma esta cuestión.
-
-            Además de las multas económicas, la ordenanza también establece el decomiso del material de pirotecnia en manos de infractores, lo que busca eliminar cualquier incentivo para el incumplimiento de la ley. Para garantizar un cumplimiento efectivo, se contempla la inhabilitación automática de los establecimientos que vendan pirotecnia ilegal y la clausura temporal o definitiva de los locales infractores.
-
-            Para asegurarse de que la ordenanza se cumpla a cabalidad, el municipio ha anunciado un aumento en las operaciones de control y vigilancia durante la temporada de festividades. Equipos de inspectores municipales serán los encargados de monitorear la venta y el uso de pirotecnia, y tomarán medidas inmediatas contra aquellos que la utilicen de manera indebida.
-          </p>
-          <h4 className='font-semibold text-six mt-4'>Modificado por última vez en Jueves, 21 Diciembre 2023 10:30
-            Publicado en Noticias
-          </h4>
+          <img className='rounded-t-lg pb-2' src={import.meta.env.VITE_STRAPI_URL + newContent.Imagen.data.attributes.url} alt='img not fount' />
+          <h5 className='font-semibold text-six'>{formatDate(newContent.createdAt)}</h5>
+          <h1 className='font-extrabold text-3xl text-secondary my-4'>{newContent.Titulo}</h1>
+          <h4 className='italic font-semibold text-six'>{newContent.Descripcion}</h4>
+          <Markdown className='prose max-w-full text-[#616161] mt-6'>{newContent.Contenido}</Markdown>
+          <p className='font-semibold text-six mt-4'>{formatDate(newContent.updatedAt)}</p>
+          <p className='font-semibold text-six'>Publicado en Noticias</p>
         </div>
         {/* BUSCAR NOTICIAS */}
         <div className='lg:pt-8'>
